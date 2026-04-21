26ba1f1718120fed8223b6c408fc0e0a1ed6e400
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2026-04-21 03:52:12 +0000

    Implement task deletion functionality and clean up task creation code

--- main.js
+++ main.js
@@ -4,16 +4,28 @@ const taskListEl = document.querySelector("#task-list");
 let counter = 1;
 let lastTask = null;
 
+const deleteTask = (event) => {
+    const taskElementToDelete = event.target.parentElement;
+    if(lastTask == taskElementToDelete){
+        lastTask = null;
+    }
+    taskListEl.removeChild(taskElementToDelete);
+}
+
 const addNewTask = () => {
   const newTask = document.createElement(`div`);
   newTask.id = "task-" + counter;
   newTask.classList =
   "card d-flex flex-row p-2 my-2 col-3 justify-content-between align-items-center";
   newTask.innerHTML = `
-  <i class="fa-regular fa-circle-check text-primary"></i>
-  <p class="m-0">${inputTaskEl.value}!</p>
-  <i class="fa-solid fa-trash-can text-danger"></i>
-  `
+    <i class="fa-regular fa-circle-check text-primary"></i>
+    <p class="m-0">${inputTaskEl.value}</p>
+  `;
+    const deleteIcon = document.createElement("i");
+    deleteIcon.classList = "fa-solid fa-trash-can text-danger";
+    deleteIcon.onclick = deleteTask;
+    newTask.appendChild(deleteIcon);
+
     if(lastTask == null){
         inputTaskEl.value = "";
         taskListEl.appendChild(newTask);
